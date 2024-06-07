bd38df2db09526e5de30214d6536ff7d0639e166
Lovepreet <gillpreetsingh35@gmail.com>
2024-06-07 19:39:56 -0400

    employee model created

---
+++ app/Http/Controllers/CarRepairHistoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\car_repair_history;
+use App\Http\Requests\Storecar_repair_historyRequest;
+use App\Http\Requests\Updatecar_repair_historyRequest;
+
+class CarRepairHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storecar_repair_historyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(car_repair_history $car_repair_history)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(car_repair_history $car_repair_history)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatecar_repair_historyRequest $request, car_repair_history $car_repair_history)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(car_repair_history $car_repair_history)
+    {
+        //
+    }
+}
---
+++ database/factories/EmployeesFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\employees>
+ */
+class EmployeesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            
+            "employee_name" => fake() ->Name(),
+            "email" => fake() ->unique()->safeEmail(),
+            "mobile" => fake() ->unique()->phoneNumber(),
+            "address" => fake() ->address(),
+            "joining_date" => fake() ->date(),
+            // "job_role" => fake() ->jobRole()
+
+        ];
+    }
+}
