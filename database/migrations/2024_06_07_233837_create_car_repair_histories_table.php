4f4b314557251bf4a16f2653fa5b4bb6987b5f81
Lovepreet <gillpreetsingh35@gmail.com>
2024-06-09 07:09:19 -0400

    car repait history table completed

--- database/migrations/2024_06_07_233837_create_car_repair_histories_table.php
+++ database/migrations/2024_06_07_233837_create_car_repair_histories_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('car_repair_histories', function (Blueprint $table) {
             $table->id();
+            $table->integer("vin");
+            $table->date("repair_date");
+            $table->string("repair_type");
+            $table->bigInteger('employee_id')->unsigned()->index()->nullable();
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->string("parts_used");
+            $table->decimal("total_cost");
+            $table->string("note");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
