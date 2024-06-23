6a065a6c80a184bef3610147781ec2ad82a20f12
Lovepreet <gillpreetsingh35@gmail.com>
2024-06-23 19:44:49 -0400

    database default is changed to mysql

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,7 +66,7 @@ return [
             'database' => env('RDS_DB_NAME', 'laravel'),
             'username' => env('RDS_USERNAME', 'root'),
             'password' => env('RDS_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('RDS_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
             'prefix' => '',
